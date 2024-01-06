6f7ed4205d9937db32226ec9a2d4dcc66975ec6a
sportsmattress <154449583+sportsmattress@users.noreply.github.com>
2024-01-06 11:58:37 -0500

    Add files via upload

---
+++ etch-a-sketch/index.js
@@ -0,0 +1,13 @@
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i=0; i<256; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = "blue";
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateBoard(16);
